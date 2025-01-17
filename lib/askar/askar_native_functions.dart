ebd853ea4e68089a1ff0d9381418575c755b1f99
Bruno Brasil <bruno-brasil.ferreira@serpro.gov.br>
2025-01-17 10:16:56 -0300

    add functions askarSessionStart and askarStoreProvision

--- lib/askar/askar_native_functions.dart
+++ lib/askar/askar_native_functions.dart
@@ -1,10 +1,51 @@
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
-// Define the path to the shared library
-final DynamicLibrary nativeLib = DynamicLibrary.open('android/app/src/main/libaries_askar/libaries_askar.so');
+typedef AskarVersionNative = Pointer<Utf8> Function();
+
+typedef AskarSessionStartNative = Int32 Function(
+  Int32 handle,
+  Pointer<Utf8> profile,
+  Int8 as_transaction,
+  Pointer<NativeFunction<Void Function(Int32, Int32, Int32)>> cb,
+  Int32 cb_id,
+);
+
+typedef AskarStoreProvisionNative = Int32 Function(
+  Pointer<Utf8> spec_uri,
+  Pointer<Utf8> key_method,
+  Pointer<Utf8> pass_key,
+  Pointer<Utf8> profile,
+  Int8 recreate,
+  Pointer<NativeFunction<Void Function(Int32, Int32, Pointer<Void>)>> cb,
+  Int32 cb_id,
+);
+
+final DynamicLibrary nativeLib = DynamicLibrary.open(
+    'android/app/src/main/libaries_askar/libaries_askar.so');
 
 final Pointer<Utf8> Function() nativeAskarVersion = nativeLib
-    .lookup<NativeFunction<Pointer<Utf8> Function()>>('askar_version')
+    .lookup<NativeFunction<AskarVersionNative>>('askar_version')
     .asFunction();
 
+final int Function(
+  int handle,
+  Pointer<Utf8> profile,
+  int as_transaction,
+  Pointer<NativeFunction<Void Function(Int32, Int32, Int32)>> cb,
+  int cb_id,
+) nativeAskarSessionStart = nativeLib
+    .lookup<NativeFunction<AskarSessionStartNative>>('askar_session_start')
+    .asFunction();
+
+final int Function(
+  Pointer<Utf8> spec_uri,
+  Pointer<Utf8> key_method,
+  Pointer<Utf8> pass_key,
+  Pointer<Utf8> profile,
+  int recreate,
+  Pointer<NativeFunction<Void Function(Int32, Int32, Pointer<Void>)>> cb,
+  int cb_id,
+) nativeAskarStoreProvision = nativeLib
+    .lookup<NativeFunction<AskarStoreProvisionNative>>('askar_store_provision')
+    .asFunction();
--- lib/askar/askar_wrapper.dart
+++ lib/askar/askar_wrapper.dart
@@ -1,8 +1,63 @@
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
+import 'package:import_so_libaskar/askar/askar_error_code.dart';
 import 'askar_native_functions.dart';
 
 String askarVersion() {
   Pointer<Utf8> resultPointer = nativeAskarVersion();
   return resultPointer.toDartString();
+}
+
+ErrorCode askarSessionStart(
+  int handle,
+  String profile,
+  int asTransaction,
+  Pointer<NativeFunction<Void Function(Int32, Int32, Int32)>> cb,
+  int cbId,
+) {
+  final profilePointer = profile.toNativeUtf8();
+
+  final result = nativeAskarSessionStart(
+    handle,
+    profilePointer,
+    asTransaction,
+    cb,
+    cbId,
+  );
+
+  calloc.free(profilePointer);
+
+  return intToErrorCode(result);
+}
+
+ErrorCode askarStoreProvision(
+  String specUri,
+  String keyMethod,
+  String passKey,
+  String profile,
+  int recreate,
+  Pointer<NativeFunction<Void Function(Int32, Int32, Pointer<Void>)>> cb,
+  int cbId,
+) {
+  final specUriPointer = specUri.toNativeUtf8();
+  final keyMethodPointer = keyMethod.toNativeUtf8();
+  final passKeyPointer = passKey.toNativeUtf8();
+  final profilePointer = profile.toNativeUtf8();
+
+  final result = nativeAskarStoreProvision(
+    specUriPointer,
+    keyMethodPointer,
+    passKeyPointer,
+    profilePointer,
+    recreate,
+    cb,
+    cbId,
+  );
+
+  calloc.free(specUriPointer);
+  calloc.free(keyMethodPointer);
+  calloc.free(passKeyPointer);
+  calloc.free(profilePointer);
+
+  return intToErrorCode(result);
 }
